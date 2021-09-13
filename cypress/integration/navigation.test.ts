569360cba76bd9ba2b8ccbf12ec2707336ad30cb
Justin Yum <justinyum98@gmail.com>
2021-09-13 15:57:48 -0700

    passed integration tests

--- cypress/integration/navigation.test.ts
+++ cypress/integration/navigation.test.ts
@@ -1,14 +1,14 @@
 describe('Navigation', () => {
   it('should navigate to about page from the home page', () => {
     // ARRANGE
+    cy.viewport(1024, 1000)
     cy.visit('http://localhost:3000/');
 
     // ACT
     cy.get('a[id="header-navbar-navlinks-about"]').click();
 
     // ASSERT
-    cy.url().should('include', '/about');
-    cy.get('div').contains('About page content here');
+    cy.url().should('include', '/#about');
   });
 });
 
