7382795fd65d975cc7bf4a78e2e880e8351087bf
Justin Yum <justinyum98@gmail.com>
2021-09-18 16:29:50 -0700

    tested navigation to about and work sections

--- cypress/integration/navigation.test.ts
+++ cypress/integration/navigation.test.ts
@@ -1,14 +1,29 @@
 describe('Navigation', () => {
-  it('should navigate to about page from the home page', () => {
-    // ARRANGE
-    cy.viewport(1024, 1000)
+  beforeEach(() => {
+    // Set viewport to 'lg' 
+    cy.viewport(1024, 1000);
+  });
+
+  it('should navigate to about section on the home page when clicking "About" button', () => {
+    // Arrange
     cy.visit('http://localhost:3000/');
 
-    // ACT
+    // Act
     cy.get('a[id="header-navbar-navlinks-about"]').click();
 
-    // ASSERT
+    // Assert
     cy.url().should('include', '/#about');
+  });
+
+  it('should navigate to work section from the home page when clicking "Work" button', () => {
+    // Arrange
+    cy.visit('http://localhost:3000/');
+
+    // Act
+    cy.get('a[id="header-navbar-navlinks-work"]').click();
+
+    // Assert
+    cy.url().should('include', '/#work');
   });
 });
 
