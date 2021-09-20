efd658b19ea8414179359629ba2d3e2efa589205
Justin Yum <justinyum98@gmail.com>
2021-09-20 16:03:41 -0700

    added mobile sidebar menu

--- cypress/integration/navigation.test.ts
+++ cypress/integration/navigation.test.ts
@@ -1,12 +1,12 @@
 describe('Navigation', () => {
   beforeEach(() => {
-    // Set viewport to 'lg' 
+    // Set viewport to 'lg'
     cy.viewport(1024, 1000);
+    cy.visit('http://localhost:3000');
   });
 
-  it('should navigate to about section on the home page when clicking "About" button', () => {
+  it('should navigate to about section on the home page when clicking "About" link', () => {
     // Arrange
-    cy.visit('http://localhost:3000/');
 
     // Act
     cy.get('a[id="header-navbar-navlinks-about"]').click();
@@ -15,15 +15,54 @@ describe('Navigation', () => {
     cy.url().should('include', '/#about');
   });
 
-  it('should navigate to work section from the home page when clicking "Work" button', () => {
+  it('should navigate to work section from the home page when clicking "Work" link', () => {
     // Arrange
-    cy.visit('http://localhost:3000/');
 
     // Act
     cy.get('a[id="header-navbar-navlinks-work"]').click();
 
     // Assert
     cy.url().should('include', '/#work');
+  });
+
+  it('should navigate to projects section from the home page when clicking "Projects" link', () => {
+    // Arrange
+
+    // Act
+    cy.get('a[id="header-navbar-navlinks-projects"]').click();
+
+    // Assert
+    cy.url().should('include', '/#projects');
+  });
+
+  it('should navigate to projects section from the home page when clicking "Contact" link', () => {
+    // Arrange
+
+    // Act
+    cy.get('a[id="header-navbar-navlinks-contact"]').click();
+
+    // Assert
+    cy.url().should('include', '/#contact');
+  });
+
+  it('should navigate to Github profile page from the home page when clicking the Github icon', () => {
+    // Arrange
+
+    // Act
+    cy.get('a[id="header-navbar-socials-github"]').click();
+
+    // Assert
+    cy.url().should('eq', 'https://github.com/justinyum98');
+  });
+
+  it('should navigate to LinkedIn profile page from the home page when clicking the LinkedIn icon', () => {
+    // Arrange
+
+    // Act
+    cy.get('a[id="header-navbar-socials-linkedin"]').click();
+
+    // Assert
+    cy.url().should('eq', 'https://www.linkedin.com/in/justinyum98');
   });
 });
 
--- src/components/HeaderNavbar.tsx
+++ src/components/HeaderNavbar.tsx
@@ -2,14 +2,23 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import IconButton from '@mui/material/IconButton';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import Link from './Link';
 import LogoIcon from '../icons/LogoIcon';
 
 const HeaderNavbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   const navLinks = [
     {
       key: '0',
@@ -47,7 +56,7 @@ const HeaderNavbar = () => {
       id: 'header-navbar-socials-linkedin',
       icon: <LinkedInIcon />,
       link: 'https://www.linkedin.com/in/justinyum98',
-      ariaLabel: 'Linkedin social link',
+      ariaLabel: 'LinkedIn social link',
     },
     {
       key: '1',
@@ -59,135 +68,139 @@ const HeaderNavbar = () => {
   ];
 
   return (
-    <AppBar
-      position="sticky"
-      color="inherit"
-      elevation={0}
-      sx={{
-        paddingY: 1,
-        paddingX: {
-          lg: 3,
-        },
-      }}
-      aria-label="Header navigation bar"
-    >
-      <Toolbar
+    <>
+      <AppBar
+        position="sticky"
+        color="inherit"
+        elevation={0}
         sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          paddingY: 1,
+          paddingX: {
+            lg: 3,
+          },
         }}
+        aria-label="Header navigation bar"
       >
-        <IconButton sx={{ mr: 5 }} color="primary" size="large">
-          <LogoIcon />
-        </IconButton>
-        <Box
+        <Toolbar
           sx={{
-            display: {
-              xs: 'none',
-              lg: 'flex',
-            },
-            flexGrow: 1,
+            display: 'flex',
             flexDirection: 'row',
-            gap: '4rem',
+            justifyContent: 'space-between',
           }}
         >
-          {navLinks.map(({ key, id, name, route, ariaLabel }) => (
-            <Link
-              key={key}
-              id={id}
-              href={route}
-              color="primary"
-              fontWeight="medium"
-              underline="hover"
-            >
-              {name}
-            </Link>
-          ))}
-        </Box>
+          <IconButton color="primary" size="large">
+            <LogoIcon />
+          </IconButton>
+          <IconButton
+            id="header-navbar-open-sidebar-button"
+            sx={{
+              display: {
+                lg: 'none',
+              },
+            }}
+            color="primary"
+            size="large"
+            onClick={() => setIsOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box
+            sx={{
+              display: {
+                xs: 'none',
+                lg: 'flex',
+              },
+              ml: 5,
+              flexGrow: 1,
+              flexDirection: 'row',
+              gap: '4rem',
+            }}
+          >
+            {navLinks.map(({ key, id, name, route, ariaLabel }) => (
+              <Link
+                key={key}
+                id={id}
+                href={route}
+                color="primary"
+                fontWeight="medium"
+                underline="hover"
+                aria-label={ariaLabel}
+              >
+                {name}
+              </Link>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: {
+                xs: 'none',
+                lg: 'flex',
+              },
+              flexDirection: 'row-reverse',
+              gap: '2rem',
+            }}
+          >
+            {socialLinks.map(({ key, id, icon, link, ariaLabel }) => (
+              <IconButton
+                key={key}
+                id={id}
+                href={link}
+                color="primary"
+                size="large"
+                aria-label={ariaLabel}
+              >
+                {icon}
+              </IconButton>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        id="mobile-sidebar-menu"
+        sx={{
+          display: {
+            xs: 'flex',
+            lg: 'none',
+          },
+        }}
+        anchor="right"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
         <Box
-          sx={{
-            display: {
-              xs: 'none',
-              lg: 'flex',
-            },
-            flexDirection: 'row-reverse',
-            gap: '2rem',
-          }}
+          role="presentation"
+          sx={{ px: 8, pt: '50%' }}
+          onClick={() => setIsOpen(false)}
         >
-          {socialLinks.map(({ key, id, icon, link, ariaLabel }) => (
-            <IconButton
-              key={key}
-              id={id}
-              href={link}
-              color="primary"
-              size="large"
-            >
-              {icon}
-            </IconButton>
-          ))}
+          <List>
+            {navLinks.map(({ key, id, name, route, ariaLabel }) => (
+              <Link
+                key={key}
+                id={id}
+                href={route}
+                aria-label={ariaLabel}
+                underline="none"
+              >
+                <ListItemButton sx={{ px: 7, py: 2 }}>
+                  <ListItemText primary={name} />
+                </ListItemButton>
+              </Link>
+            ))}
+            <Divider sx={{ my: 4 }} />
+            {socialLinks.map(({ key, id, icon, link, ariaLabel }) => (
+              <Link key={key} id={id} href={link} aria-label={ariaLabel}>
+                <ListItemButton sx={{ pl: 9, py: 2 }}>
+                  <ListItemIcon sx={{ color: 'primary.main' }}>
+                    {icon}
+                  </ListItemIcon>
+                </ListItemButton>
+              </Link>
+            ))}
+          </List>
         </Box>
-      </Toolbar>
-    </AppBar>
+      </Drawer>
+    </>
   );
-
-  // return (
-  //   <div className="bg-fwTan text-fwOrange sticky top-0 w-full">
-  //     <nav
-  //       id="header-navbar"
-  //       className="w-full flex flex-row justify-between items-center px-4 py-2 lg:px-12 lg:py-4 lg:gap-x-4"
-  //       role="navigation"
-  //       aria-label="Header navigation bar"
-  //     >
-  //       <Link href="/">
-  //         <a className="pt-2 px-2 rounded-full hover:bg-white">
-  //           <img src="/logo.svg" alt="(Logo)" width={48} height={48} />
-  //         </a>
-  //       </Link>
-  //       <button
-  //         id="header-navbar-open-sidebar-button"
-  //         className="p-2 lg:hidden"
-  //         onClick={onMenuClick}
-  //       >
-  //         <BiMenuAltRight size={48} />
-  //       </button>
-  //       <div
-  //         id="header-navbar-navlinks"
-  //         className="hidden font-sans lg:flex lg:flex-auto lg:gap-x-12 lg:ml-6"
-  //       >
-  //         {navLinks.map(({ key, id, name, route, ariaLabel }) => (
-  //           <Link key={key} href={route}>
-  //             <a
-  //               id={id}
-  //               role="link"
-  //               aria-label={ariaLabel}
-  //               className="lg:px-2 lg:py-1 lg:text-base hover:text-fwRed"
-  //             >
-  //               {name}
-  //             </a>
-  //           </Link>
-  //         ))}
-  //       </div>
-  //       <div
-  //         id="header-navbar-socials"
-  //         className="hidden lg:flex lg:flex-auto lg:flex-row-reverse lg:gap-x-12"
-  //       >
-  //         {socialLinks.map(({ key, id, icon, link, ariaLabel }) => (
-  //           <a
-  //             key={key}
-  //             id={id}
-  //             href={link}
-  //             role="link"
-  //             aria-label={ariaLabel}
-  //             className="p-1"
-  //           >
-  //             {icon}
-  //           </a>
-  //         ))}
-  //       </div>
-  //     </nav>
-  //   </div>
-  // );
 };
 
 export default HeaderNavbar;
