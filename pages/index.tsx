33d45a99386ae3da7648cdef4f0dfe7fb27a1a74
Justin Yum <justinyum98@gmail.com>
2021-09-22 11:19:58 -0700

    made IntroSection component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 
 import HeaderNavbar from '../src/components/HeaderNavbar';
+import IntroSection from '../src/components/IntroSection';
 
 const Index = () => {
   return (
@@ -21,77 +21,7 @@ const Index = () => {
           },
         }}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            py: {
-              xs: 6,
-              md: 10,
-              lg: 20,
-            },
-          }}
-        >
-          <Typography variant="h6" component="div" color="textSecondary">
-            Hi, my name is
-          </Typography>
-          <Box
-            sx={{
-              pb: {
-                xs: 1,
-                lg: 2,
-              },
-            }}
-          >
-            <Typography
-              variant="h1"
-              sx={{
-                fontSize: {
-                  xs: 'h3.fontSize',
-                  md: 'h2.fontSize',
-                },
-              }}
-              component="div"
-              fontWeight="bold"
-            >
-              Justin Yum.
-            </Typography>
-            <Typography
-              variant="h3"
-              sx={{
-                fontSize: {
-                  xs: 'h4.fontSize',
-                  md: 'h3.fontSize',
-                },
-              }}
-              component="div"
-              fontWeight="bold"
-            >
-              Software Engineer.
-            </Typography>
-          </Box>
-          <Typography variant="subtitle1" paragraph fontWeight="medium">
-            I develop (and sometimes design) both web and mobile experiences.
-            <br />
-            Currently, I am a{' '}
-            <Typography
-              variant="inherit"
-              component="span"
-              color="textSecondary"
-            >
-              junior undergraduate student
-            </Typography>{' '}
-            working part-time at{' '}
-            <Typography
-              variant="inherit"
-              component="span"
-              color="textSecondary"
-            >
-              ASML
-            </Typography>
-            .
-          </Typography>
-        </Box>
+        <IntroSection />
       </Container>
     </Box>
   );
---
+++ src/components/IntroSection.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const IntroSection = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        py: {
+          xs: 6,
+          md: 10,
+          lg: 20,
+        },
+      }}
+    >
+      <Typography variant="h6" component="div" color="textSecondary">
+        Hi, my name is
+      </Typography>
+      <Box
+        sx={{
+          pb: {
+            xs: 1,
+            lg: 2,
+          },
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            fontSize: {
+              xs: 'h3.fontSize',
+              md: 'h2.fontSize',
+            },
+          }}
+          component="div"
+          fontWeight="bold"
+        >
+          Justin Yum.
+        </Typography>
+        <Typography
+          variant="h3"
+          sx={{
+            fontSize: {
+              xs: 'h4.fontSize',
+              md: 'h3.fontSize',
+            },
+          }}
+          component="div"
+          fontWeight="bold"
+        >
+          Software Engineer.
+        </Typography>
+      </Box>
+      <Typography variant="subtitle1" paragraph fontWeight="medium">
+        I develop (and sometimes design) both web and mobile experiences.
+        <br />
+        Currently, I am a{' '}
+        <Typography variant="inherit" component="span" color="textSecondary">
+          junior undergraduate student
+        </Typography>{' '}
+        working part-time at{' '}
+        <Typography variant="inherit" component="span" color="textSecondary">
+          ASML
+        </Typography>
+        .
+      </Typography>
+    </Box>
+  );
+};
+
+export default IntroSection;
