c888bcfa06404e40af455fcc106bef232f815aaa
Justin Yum <justinyum98@gmail.com>
2021-09-30 12:08:02 -0700

    fixed lint error

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,15 +3,15 @@ import Typography from '@mui/material/Typography';
 import SectionContainer from './SectionContainer';
 
 const ProjectsSection = () => {
-  const projects = [
-    {
-      name: 'First Iteration of Personal Website',
-      description:
-        "My first personal project ever. On May 2019, I made it my goal to start and finish creating my own personal website from scratch. After some growing pains and even scrapping the first rendition completely, I finally finished the website on December 2019. Though it's not my best work, I can proudly say that this was the start of my web development journey.",
-      pictureUrl: '',
-      url: 'https://justinyum.netlify.app/',
-    },
-  ];
+  // const projects = [
+  //   {
+  //     name: 'First Iteration of Personal Website',
+  //     description:
+  //       "My first personal project ever. On May 2019, I made it my goal to start and finish creating my own personal website from scratch. After some growing pains and even scrapping the first rendition completely, I finally finished the website on December 2019. Though it's not my best work, I can proudly say that this was the start of my web development journey.",
+  //     pictureUrl: '',
+  //     url: 'https://justinyum.netlify.app/',
+  //   },
+  // ];
 
   return (
     <SectionContainer id="projects">
